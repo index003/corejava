0c01279495ed6b72d4cb4448d4fb6a964c2d4252
7978766721@wcz <index003@sina.cn>
2022-03-31 15:21:19 +0800

    update ch08 generics

---
+++ corejava/src/v1ch08/pair3/Manager.java
@@ -0,0 +1,23 @@
+package v1ch08.pair3;
+
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
+}
